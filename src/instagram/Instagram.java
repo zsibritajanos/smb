c36b637c3e62c88569c86b1b138bfd43c9a42503
Zsibrita Janos <zsibritajanos@Janos-MacBook-Pro.local>
2017-01-05 13:02:45 +0100

    Instagram.java initial steps

---
+++ src/instagram/Instagram.java
@@ -0,0 +1,19 @@
+
+// permission
+// https://www.instagram.com/developer/clients/4512b21283f34219afacc3c20c13c6ec/edit/
+//
+//        client_ID = "4512b21283f34219afacc3c20c13c6ec"
+//        client_secret = "2d478d643602420a92ee2b7c2db0f552"
+//        access_token = "2215977847.1677ed0.f2d61ab501634f5ca6b4030cbd706b51"
+//        redirect_url = "https://www.facebook.com/djcorvinmusic/"
+//
+//        access_token = "2215977847.4512b21.11b90f7dc78645bdaf3f0cc870ac6fe6"
+//
+//
+//        user_info_url = "https://www.instagram.com/djviktoriametzker/?__a=1"
+//
+//        viki_id = "26859739"
+//        get_viki_media_url = "https://api.instagram.com/v1/users/26859739/media/recent?access_token=2215977847.4512b21.11b90f7dc78645bdaf3f0cc870ac6fe6"
+//        get_my_media_url = "https://api.instagram.com/v1/users/2215977847/media/recent?access_token=2215977847.4512b21.11b90f7dc78645bdaf3f0cc870ac6fe6"
+//
+//        url = "https://instagram.com/oauth/authorize/?client_id=" + client_ID + "&redirect_uri=" + redirect_url + "&scope=public_content&response_type=token"
